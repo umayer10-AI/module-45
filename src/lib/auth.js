0ddf68e32d2bdc843377633f36f8d835868eb60f
umayer10-AI <mdumayer10@gmail.com>
2026-04-21 19:02:48 +0600

    add

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,10 +2,13 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
+    emailAndPassword: { 
+        enabled: true, 
+    },
     database: mongodbAdapter(db, {
         // Optional: if you don't provide a client, database transactions won't be enabled.
         client,
